a59f32ff05b19e06e0f2015e5d2499c3b1232761
Ettore Rossatto <ettore.rossatto@ciadaconsulta.com.br>
2020-04-30 23:27:51 -0300

    Countdown style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  private _nowDate = moment().format("DD/MM/YYYY hh");
+  private _launchDate = moment('20/05/2020 00:00', "DD/MM/YYYY hh");
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  public countdownTimer(value: string): string {
+    const timeDifference = moment(this._launchDate, "DD/MM/YYYY hh").diff(moment( this._nowDate, "DD/MM/YYYY hh"));
+
+    switch (value) {
+      case 'months':
+        return moment.utc(timeDifference).format("MM");
+      case 'days':
+        return moment.utc(timeDifference).format("DD");
+      case 'hours':
+        return moment.utc(timeDifference).format("hh");
+    };
+  }
+
+  onSubmit(form: NgForm) {
+    console.log('-->: ', form.form.value);
   }
 }
