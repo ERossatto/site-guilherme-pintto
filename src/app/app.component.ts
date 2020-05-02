c851a2d06334df6343164f5b7836bb9654b03aa8
Ettore Rossatto <ettore.rossatto@ciadaconsulta.com.br>
2020-05-02 01:02:07 -0300

    Create popup contact us

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 import * as moment from 'moment';
 
@@ -10,8 +9,33 @@ import * as moment from 'moment';
 })
 export class AppComponent implements OnInit {
 
+  @ViewChild('textArea', {static: false}) textArea: string = '';
+
   private _nowDate = moment().format("DD/MM/YYYY hh");
   private _launchDate = moment('20/05/2020 00:00', "DD/MM/YYYY hh");
+
+  public links = {
+    youtubeLink:  'https://www.youtube.com/user/guipintto',
+    instagramLink: 'https://www.instagram.com/guipintto/',
+    twitterLink: 'https://twitter.com/guipintto',
+    facebookLink: 'https://www.facebook.com/guipinttto/',
+    bookOneLink: 'https://www.amazon.com.br/%C3%B3bvio-tamb%C3%A9m-precisa-ser-dito/dp/8542216369/ref=pd_lpo_14_t_0/138-6324468-6727742?_encoding=UTF8&pd_rd_i=8542216369&pd_rd_r=ef9defb1-205c-4861-9906-ec111033d752&pd_rd_w=o9fuw&pd_rd_wg=coT4J&pf_rd_p=e7e26e7d-6256-4aae-92f9-7ffa337ed626&pf_rd_r=3DM6S92R1EW1GS0HTQ8X&psc=1&refRID=3DM6S92R1EW1GS0HTQ8X',
+    bookTwoLink: 'https://www.amazon.com.br/Seja-amor-vida-Guilherme-Pintto/dp/8542212991',
+  };
+
+  public personInfo = {
+    name: '',
+    email: '',
+    whatsapp: ''
+  };
+
+  public contactMessage = {
+    name: '',
+    content: this.textArea
+  }
+
+  public hideConfirmationMsg: boolean = true;
+  public showContactPopup: boolean = true; //mock retornar para false
 
   constructor() { }
 
@@ -20,8 +44,6 @@ export class AppComponent implements OnInit {
 
   public countdownTimer(value: string): string {
     const timeDifference = moment(this._launchDate, "DD/MM/YYYY hh").diff(moment( this._nowDate, "DD/MM/YYYY hh"));
-
-    console.log('-->: ', timeDifference);
 
     switch (value) {
       case 'months':
@@ -33,7 +55,19 @@ export class AppComponent implements OnInit {
     };
   }
 
-  onSubmit(form: NgForm) {
-    console.log('-->: ', form.form.value);
+  public onSubmit() {
+    console.log('-->: ', this.personInfo);
+    this.hideConfirmationMsg = false;
+    this.personInfo.name = '';
+    this.personInfo.email = '';
+    this.personInfo.whatsapp = '';
+  }
+
+  public openContactForm() {
+    this.showContactPopup = true;
+  }
+
+  public sendContactMessage() {
+
   }
 }
